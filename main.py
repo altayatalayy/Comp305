2f10a41ef6052263a2528a933b9260b37adfd204
Altay Atalay <altayatalay1998@gmail.com>
2021-05-20 13:52:09 +0300

    added mst and drop until minimum to the algo

--- main.py
+++ main.py
@@ -7,10 +7,68 @@ Members:
 Project Name: Pokemon
 '''
 
-def network_from_file(file_name : str) -> (dict, int):
+import sys
+
+def check_correctness(selected_cities, road_network):
+    # check if the solution satisfies the problem
+    has_center = {k : False for k in road_network.keys()}
+    near_center = dict(has_center)
+
+    #Mark cities with centers
+    for city_name in selected_cities:
+        has_center[city_name] = True#if there is pokecenter
+
+    # if any of the adjacent cities has center mark this city as near center
+    for k in road_network.keys():
+        for adj_city in road_network[k]:
+            if has_center[adj_city]:
+                near_center[k] = True
+
+    #print(f'{has_center}, {near_center}')
+    rv = all([h or n for h,n in zip(has_center.values(), near_center.values())])
+    return rv
+
+def minKey(key, mstSet):
+    # util function for prim algo
+    #print(f'minkey key = {key}')
+    _min = 100
+    for v in range(len(key)):
+        if 1 < _min and mstSet[v] == False:
+            _min = key[v]
+            min_index = v
+
+    return min_index
+
+def mst(tree : dict):
+    #prims algorithm, returns a list of parents
+    n = len(tree.keys())
+    key = [100] * n
+    parent = [None] * n
+    key[0] = 0
+    mstSet = [False] * n
+    parent[0] = -1
+    for i in range(n):
+        u = minKey(key, mstSet)
+        mstSet[u] = True
+        for j in range(n):
+            if list(tree.keys())[i] in  tree[list(tree.keys())[j]] and mstSet[j] == False and key[j] > 1:
+                #print(i, j, u)
+                key[j] = 1
+                parent[j] = u
+
+    rv = []
+    #Convert indexes to city names
+    for i in parent:
+        if i == -1:
+            continue
+        if list(tree.keys())[i] not in rv:
+            rv.append(list(tree.keys())[i] )
+
+    return rv
+
+def network_from_file(file_name : str) -> dict:
     '''Create road network from input file
     '''
-    num_cities:int = 0
     keys:list = [] # Cities
     values:'2d list' = [] # Connected Cities 2d list
 
@@ -29,7 +87,37 @@ def network_from_file(file_name : str) -> (dict, int):
             values.append(connected_cities)
 
     road_network:dict = {k : v for k,v in zip(keys, values)} # Create the road network usin keys and values
-    return road_network, num_cities
+    return road_network
+
+def helper(selected_cities, road_network):
+    # Recursive function to get a soluttion with minimum number of cities
+    #print(len(selected_cities))
+    if not type(selected_cities) in [list]:
+        return
+
+    if len(selected_cities) == 0:
+        return
+
+    if not check_correctness(selected_cities, road_network):
+        return
+
+    _min = len(selected_cities)
+    _city = None
+    _rv = selected_cities
+    for city in selected_cities:
+        sc = list(selected_cities)# create copy of selected cities
+        sc.remove(city)
+        rv = helper(sc, road_network)
+        if rv == None:
+            continue
+        if len(rv) < _min:
+            _min = len(rv)
+            _rv = rv
+            break
+
+    #print(_rv)
+    return _rv
+
 
 def can_be_made_tournament_ready(road_network : dict, num_cities : int) -> list:
     '''The Algorithm
@@ -42,13 +130,23 @@ def can_be_made_tournament_ready(road_network : dict, num_cities : int) -> list:
     if num_cities == 0:
         return rv
 
+    mst_list = mst(road_network)#Minimum Spanning Tree of our network
+
+    # Get minimum number of cities
+    min_list = helper(mst_list, road_network)
+    if len(min_list) <= num_cities:
+        rv = min_list
+
     return rv
 
 
 if __name__ == '__main__':
-    road_network, num_cities = network_from_file('pokemon-center-test1.txt')
+    num_cities:int = 10
+    road_network = network_from_file('pokemon-center-test1.txt')
     print(road_network)
     result = can_be_made_tournament_ready(road_network, num_cities)
     print(f'result = {result}')
+
+    #print(mst(road_network))
 
 
--- test.py
+++ test.py
@@ -1,15 +1,38 @@
 
 import unittest
 
-import main
-
-f = main.can_be_made_tournament_ready
+from main import *
+from main import can_be_made_tournament_ready as algo
 
 class TestAlgo(unittest.TestCase):
 
-    def test(self):
-        # test negative num cities
-        self.assertRaises(ValueError, f, {}, -1)
+    def test_inputs(self):
+        print('testing invalid values')
+        self.assertRaises(ValueError, algo, {}, -1)
+
+    def test_input_from_fille(self):
+        print('test input from file')
+        road_network = network_from_file('pokemon-center-test1.txt')
+        self.assertEqual(type(road_network), dict)
+
+    def test_mst(self):
+        print('check prims algo')
+        road_network = network_from_file('pokemon-center-test1.txt')
+        _mst = mst(road_network)
+        self.assertEqual(check_correctness(_mst, road_network), True)
+
+    def test_correctness(self):
+        print('testing correctness')
+        num_cities = 10
+        road_network = network_from_file('pokemon-center-test1.txt')
+        result = can_be_made_tournament_ready(road_network, num_cities)# get nodes to place center
+
+        #Check results type
+        self.assertEqual(type(result), list)
+        for r in result:
+            self.assertEqual(type(r), str)
+
+        self.assertEqual(check_correctness(result, road_network), True)
 
 
 if __name__ == '__main__':
